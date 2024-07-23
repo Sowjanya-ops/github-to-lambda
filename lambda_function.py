20e3af172570993596d03d5311ec7e2e40a3ffba
sowjanyan27 <sowjanyan@newmeksolutions.com>
2024-07-23 18:11:28 +0530

    itr-3

--- lambda_function.py
+++ lambda_function.py
@@ -1,2 +1,8 @@
+import json
 
-print("lambda-to git-aatempt-2")
+def lambda_handler(event, context):
+    # TODO implement
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Hello from Lambda from cicd!')
+    }
